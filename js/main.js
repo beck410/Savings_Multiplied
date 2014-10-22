38d50d6214f7c2f31e55a8a4238db3192ef51be8
beck410 <beck410@hotmail.com>
2014-10-21 21:19:51 -0500

    Added product information(json) for buy.html

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@
        	 classIndex = i;
     	  }
 
-			  if ( classIndex ) {
+        if ( classIndex ) {
       	  classes.splice(existingIndex, 1);
 			  }
     	  else {
@@ -44,26 +44,77 @@
 })();
 
 (function (){
+  //var 
+  var $ul = document.querySelector('.product-items');
+
   //create json function
+  function getJSON(url, cb){
     //create new xhr request
+    xhr = new XMLHttpRequest();
     //open request
+    xhr.open('GET', url);
     //create function to retrieve data - execute cb(what to do with data)
+    xhr.onload = function(){
+      cb(JSON.parse(xhr.responseText));
+    };
     //send request
+    xhr.send();
+  }
 
   //execute json function -args: data url & create callback
-    //callback fn  
-      //create new array var to put data in
-      //run a for each loop 
-        //put append function into a local var
-        //append local var to #product-list ul 
+  getJSON('https://savingsmultipliedssh.firebaseio.com/itemlist.json', function(data){
+    //create new array var to put data in
+    productArray = data;
+    //run a for each loop
+    productArray.forEach(appendListItem) 
+      //put append function into a local var
+      //append local var to #product-list ul 
+  });
+
+  // append li to ul 
+  function appendListItem(product){
+    var liDocFrag = createListItem(product);
+    $ul.appendChild(liDocFrag); 
+  }
   
-  //  createLi funtion 
-    //create document frag var
-    //create li tag 
-    //create img tag
+  //create li funtion 
+  function createListItem(product){
     //give img tag an src attribute(arg)
+    var $li = document.createElement('li');
+    var $img = document.createElement('img');
+    var docFrag = document.createDocumentFragment();
+    var pOneDocFrag = createPOneItem(product);
+
+    $img.setAttribute('src', product.image);
     //append img to li
-    //append li to doc frag var 
+    $li.appendChild($img);
+    //append first p to li
+    $li.appendChild(pOneDocFrag)
+    //append li to doc frag var
+    docFrag.appendChild($li);
+
+    return docFrag;
+  }
+
+  //create first paragraph with span items 
+  function createPOneItem(product){
+    var docFrag = document.createDocumentFragment();
+    var $p = document.createElement('p');
+    var $spanOne = document.createElement('span')    
+    var $spanTwo = document.createElement('span')
+    //add inner text to span1
+    $spanOne.textContent = product.title + " " + product.price;
+    //add inner text to span2
+    $spanTwo.textContent = product.seller + " " + product.remaining;
+    //append span1 to p1 tag
+    $p.appendChild($spanOne);
+    //append span2 to p1 tag
+    $p.appendChild($spanTwo);
+    //append p to doc frag var
+    docFrag.appendChild($p);
+
+    return docFrag;
+  }
 })();
 
 
